42b12a58fa1f049715b14a3d8c441a12ffaa52d7
Patrick Vibild <patrickvibild@gmail.com>
2023-03-27 17:09:31 +0900

    update cuda 11 for arm ubuntu 20.04 installer.

--- installers/cuda11.sh
+++ installers/cuda11.sh
@@ -1,15 +1,2 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-
-#add public keys
-
-# Old key
-#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-
-# new key, added 2022-04-25 22:52
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-
-sudo apt update
-sudo apt install cuda-toolkit-11-6
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux_sbsa.run
+sudo sh cuda_11.0.2_450.51.05_linux_sbsa.run
